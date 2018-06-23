472db1df89eda2f1c5d77e8b821fce5b143e014c
slice <ryaneft@gmail.com>
2018-06-22 21:59:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# discord-package-exporter
+
+This repository houses a Python 3 script that will export all messages from
+your [Discord data package][ddp] into a [Postgresql][postgres] database.
+Instructions showing the process of obtaining your data package are [here][ob].
+
+[postgres]: https://www.postgresql.org/
+[ob]:
+https://support.discordapp.com/hc/en-us/articles/360004027692-Requesting-a-Copy-of-your-Data\
+[ddp]: https://support.discordapp.com/hc/en-us/articles/360004957991-Your-Discord-Data-Package
+
+## Usage
+
+This script uses psycopg2 to connect to the database. Install dependencies
+with `pip`:
+
+```sh
+$ pip3 install -U -r requirements.txt
+```
+
+Or, if you'd prefer, just install `psycopg2` manually with `pip3 install
+psycopg2`.
+
+This script assumes that you already have a Postgresql server setup on your
+local machine. The script will automatically create any required tables for
+you, but you still need to create a database (you can name it `discord`):
+
+```sh
+$ createdb discord
+```
+
+Once you have created the database, run the script:
+
+```sh
+$ python3 dpe.py "$HOME/discord-data-package" "dbname=discord user=joe"
+```
+
+The first argument is a path to your Discord data package, while the second
+argument is a [psycopg2 DSN (connection string)][connstr].
+
+[connstr]: https://www.postgresql.org/docs/current/static/libpq-connect.html#LIBPQ-CONNSTRING
+
+This process can take a few minutes, depending on multiple factors. Progress
+will be shown in your terminal while data moves through the tubes.
---
+++ dpe.py
@@ -0,0 +1,137 @@
+import csv
+import datetime
+import json
+import re
+import sys
+from pathlib import Path
+
+import psycopg2
+
+__all__ = ['Exporter']
+
+STRIP_DATE_REGEX = re.compile(r'(\.\d{6})?\+00:00')
+
+
+class Exporter:
+
+    def __init__(self, package_path: Path, dsn: str):
+        self.package_path = package_path
+        self.dsn = dsn
+        self.conn = psycopg2.connect(dsn)
+        self.cur = self.conn.cursor()
+        self.index = None
+
+        self.load_index()
+
+    @property
+    def messages(self):
+        return self.package_path / 'messages'
+
+    def process_message(self, row):
+        r_id, r_isodate, r_content, _ = row
+        r_date = datetime.datetime.strptime(
+            STRIP_DATE_REGEX.sub('', r_isodate), '%Y-%m-%d %H:%M:%S'
+        )
+        return r_id, r_date, r_content
+
+    def load_index(self):
+        with open(self.messages / 'index.json') as fp:
+            self.index = json.load(fp)
+
+    def prepare(self):
+        self.cur.execute(
+            '''
+                CREATE TABLE IF NOT EXISTS messages (
+                    channel_id BIGINT,
+                    channel_type SMALLINT,
+                    channel_name TEXT,
+                    guild_id BIGINT,
+                    guild_name TEXT,
+                    recipients BIGINT[],
+                    id BIGINT PRIMARY KEY,
+                    date TIMESTAMP WITHOUT TIME ZONE,
+                    content TEXT
+                )
+            '''
+        )
+        self.conn.commit()
+
+    def insert_message(self, *, channel, message):
+        m_id, date, content = message
+
+        recipients = channel.get('recipients', None)
+        guild = channel.get('guild', {})
+        guild_id = guild.get('id', None)
+        guild_name = guild.get('name', None)
+
+        self.cur.execute(
+            '''
+                INSERT INTO messages
+                (
+                    channel_id, channel_type, channel_name,
+                    guild_id, guild_name,
+                    recipients,
+                    id, date, content
+                )
+                VALUES (
+                    %(channel_id)s, %(channel_type)s, %(channel_name)s,
+                    %(guild_id)s, %(guild_name)s,
+                    %(recipients)s,
+                    %(id)s, %(date)s, %(content)s
+                )
+                ON CONFLICT DO NOTHING
+            ''',
+            {
+                'channel_id': channel['id'],
+                'channel_type': channel['type'],
+                'channel_name': channel['name'],
+                'guild_id': guild_id,
+                'guild_name': guild_name,
+                'recipients': recipients,
+                'id': m_id,
+                'date': date,
+                'content': content,
+            }
+        )
+
+    def export(self):
+        messages = self.package_path / 'messages'
+
+        for channel_path in messages.iterdir():
+            if channel_path.is_file():
+                continue
+
+            with open(channel_path / 'channel.json') as fp:
+                channel = json.load(fp)
+                channel['name'] = self.index.get(channel['id'], None)
+
+            with open(channel_path / 'messages.csv') as fp:
+                reader = csv.reader(fp, delimiter=',')
+                rows = filter(lambda r: r[0] != 'ID', reader)
+                messages = list(map(self.process_message, rows))
+                total = len(messages)
+
+                for (index, message) in enumerate(messages):
+                    print(
+                        '>> Exporting C#{} ({}/{}){}'.format(
+                            channel_path.name, index, total, ' ' * 10
+                        ), end='\r'
+                    )
+                    self.insert_message(channel=channel, message=message)
+
+            self.conn.commit()
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    _, raw_path, dsn = sys.argv
+    path = Path(raw_path).resolve()
+
+    exporter = Exporter(path, dsn)
+    exporter.prepare()
+    exporter.export()
+    exporter.close()
+    print('All done!')
